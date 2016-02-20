dcb0a7dc8591d324ee0ea7f15a5d70c902b0cc4c
Peter Banjo <peterbanjo@gmail.com>
2016-02-20 21:38:44 +0000

    Invalid credentials

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,14 +1,12 @@
-var Gmail = require('node-gmail-api')
-		, User = require('../models/User')
+var gmailapilib = require('gmailapilib')
+    , config = gmailapilib.configuration
+    , User = require('../models/User')
 		, google = require('googleapis')
 		, _ = require('lodash')
-		, googleAuth = require('google-auth-library')
-		, auth = new googleAuth()
-		, googleSecrets = {
-			clientID: process.env.GOOGLE_ID,
-			clientSecret: process.env.GOOGLE_SECRET,
-			callbackURL: '/auth/google/callback'
-		};
+    , apikey = process.env.GOOGLE_SECRET;
+
+
+config.apikey = apikey;
 
 /**
  * GET /gmail
@@ -17,45 +15,15 @@ exports.getMail = function(req, res) {
 
   User.findById(req.user.id, function(err, user) {
 		var googleToken = _.find(user.tokens, { kind: 'google' });
-				// , gmail = new Gmail(googleToken.accessToken)
-				// , messages = gmail.messages('label:inbox', {max: 10});
 
-		// messages.on('data', function(mail) {
-		// 	res.render('mail/list', {
-		// 		title: 'Mail'
-		// 		, user: user
-		// 		, mail: mail
-		// 		, token: googleToken.accessToken
-		// 	});
-		// });
 
-		var oauth2Client = new auth.OAuth2( googleSecrets.clientId, googleSecrets.clientSecret, googleSecrets.redirectUrl);
-
-		var gmail = google.gmail('v1');
-		oauth2Client.credentials = {token: googleToken};
-		gmail.users.labels.list({
-			auth: oauth2Client,
-			userId: 'me',
-		}, function(err, response) {
-			if (err) {
-				console.log('The API returned an error: ' + err);
-				return;
-			}
-
-			res.render('mail/list', {
-				labels: response.labels
-			});
-			// var labels = response.labels;
-			// if (labels.length == 0) {
-			// 	console.log('No labels found.');
-			// } else {
-			// 	console.log('Labels:');
-			// 	for (var i = 0; i < labels.length; i++) {
-			// 		var label = labels[i];
-			// 		console.log('- %s', label.name);
-			// 	}
-			// }
-		});
-
+// userId alt, fields, includespamtrash, key, labelids, maxresults, oauthtoken, pagetoken, prettyprint, q, quotauser, userip, callback
+    gmailapilib.MessagesController.list('me', "json", null, false, apikey, null, 10, null, null, true, null, null, null, function(messages) {
+        console.log(messages);
+      res.render('mail/list', {
+        messages: messages
+      });
+    });
 	});
 }
+
