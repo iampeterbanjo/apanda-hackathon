6043fad3aa484c48035bb4a6264fbf987cdcf7f1
Peter Banjo <peterbanjo@gmail.com>
2016-02-20 20:50:37 +0000

    Still getting an error

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,6 @@
-var GoogleContacts = require('google-contacts').GoogleContacts;
-var User = require('../models/User');
-var _ = require('lodash');
+var request = require('request')
+		, User = require('../models/User')
+		, _ = require('lodash');
 
 /**
  * GET /contacts
@@ -9,18 +9,23 @@ exports.getContacts = function(req, res) {
 
   User.findById(req.user.id, function(err, user) {
 		var googleToken = _.find(user.tokens, { kind: 'google' });
-		var c = new GoogleContacts({
-			token: googleToken.accessToken
+
+		request.get('https://www.google.com/m8/feeds/contacts/default/full?alt=json&oauth_token=' + googleToken.accessToken
+		, function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				res.flash('errors', {
+					msg: error
+				});
+			} else {
+				// var contacts = JSON.parse(body);
+
+				res.render('contacts/list', {
+					title: 'Contacts'
+					, user: user
+					, contacts: body
+					, token: googleToken.accessToken
+				});
+			}
 		});
-
-		c.getContacts(function(contacts) {
-			res.render('contacts/list', {
-				title: 'Contacts'
-				, user: user
-				, contacts: contacts
-				, token: googleToken.accessToken
-			});
-		})
-
   });
 }
