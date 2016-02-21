2fe7127b5bf98aad84f13a1f2a877748d1825c21
Amulya Kumar Sahoo <amulya349@gmail.com>
2016-02-21 19:37:05 +0530

    added code to fetch contacts using Oauth

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -5,22 +5,36 @@ var _ = require('lodash');
 /**
  * GET /contacts
  */
-exports.getContacts = function(req, res) {
+exports.getContacts = function(token) {
 
-  User.findById(req.user.id, function(err, user) {
-		var googleToken = _.find(user.tokens, { kind: 'google' });
-		var c = new GoogleContacts({
-			token: googleToken.accessToken
-		});
+  // User.findById(req.user.id, function(err, user) {
+		// var googleToken = _.find(user.tokens, { kind: 'google' });
+		// var c = new GoogleContacts({
+		// 	token: googleToken.accessToken
+		// });
 
-		c.getContacts(function(contacts) {
-			res.render('contacts/list', {
-				title: 'Contacts'
-				, user: user
-				, contacts: contacts
-				, token: googleToken.accessToken
-			});
-		})
+		// c.getContacts(function(contacts) {
+		// 	res.render('contacts/list', {
+		// 		title: 'Contacts'
+		// 		, user: user
+		// 		, contacts: contacts
+		// 		, token: googleToken.accessToken
+		// 	});
+		// })
 
-  });
+  // });
+
+  //TODO Code to fetch access token from database
+
+  //code to fetch contact from google
+  var GoogleContacts = require('google-contacts').GoogleContacts;
+	var c = new GoogleContacts({
+	  token: token
+	});
+	
+	c.getContacts(function(err,cb){
+		if(err) throw err;
+		//Logging the contactlist in console
+		console.log(cb);
+	});
 }
