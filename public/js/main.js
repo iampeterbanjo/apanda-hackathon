bba0b7ad33f2235fb9ac66e32f9c5980144d87ac
Adrian Krebs <krebs-adrian@bluewin.ch>
2016-02-21 21:59:12 +0100

    added test charts

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
 
-  // Place JavaScript code here...
+    var emotionData = {
+        labels: ["Anger", "Disgust", "Fear", "Joy", "Sadness"],
+        datasets: [
+            {
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,1)",
+                data: [65, 59, 90, 81, 56]
+            }
+        ]
+    }
 
+    var myLine = new Chart(document.getElementById("emotions").getContext("2d")).Bar(emotionData);
+
+
+    var languageData = {
+        labels: ["Analytical", "Confident", "Tentative"],
+        datasets: [
+            {
+                fillColor: "rgba(220,220,220,0.5)",
+                strokeColor: "rgba(220,220,220,1)",
+                data: [30,20,85]
+            }
+        ]
+    }
+
+
+    var myLine = new Chart(document.getElementById("language").getContext("2d")).Bar(languageData);
 });
