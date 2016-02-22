f33fa5653242be65f585dc303d72525e6f680535
Peter Banjo <peterbanjo@gmail.com>
2016-02-22 04:04:26 +0000

    Show graphs for detailed personality. Alignments, spacing etc

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         ]
     }
 
-    var myLine = new Chart(document.getElementById("emotions").getContext("2d")).Bar(emotionData);
+    // var myLine = new Chart(document.getElementById("emotions").getContext("2d")).Bar(emotionData);
 
 
     var languageData = {
@@ -26,5 +26,71 @@ $(document).ready(function() {
     }
 
 
-    var myLine = new Chart(document.getElementById("language").getContext("2d")).Bar(languageData);
+    // var myLine = new Chart(document.getElementById("language").getContext("2d")).Bar(languageData);
+
+    function $$(selector) {
+      return document.querySelector(selector);
+    }
+
+    // Artists profile
+    // create radar of highest traits
+    function showRadar(traits, selector, label) {
+      var labels = [], data = [], graph
+          , traitsCanvas = $$(selector)
+          , traitsCx = traitsCanvas.getContext('2d');
+
+      traits.map(function(trait){
+        labels.push(trait.name);
+        data.push(trait.percentage * 100);
+      });
+
+      graph = {
+        labels: labels
+        , datasets: [
+          {
+            label: label
+            , fillColor: "rgba(220,220,220,0.2)"
+            , strokeColor: "rgba(220,220,220,1)"
+            , pointColor: "rgba(220,220,220,1)"
+            , pointStrokeColor: "#fff"
+            , pointHighlightFill: "#fff"
+            , pointHighlightStroke: "rgba(220,220,220,1)"
+            , data: data
+          }
+        ]
+      };
+
+      var traitsChart = new Chart(traitsCx).Radar(graph, { responsive: true, barStrokeWidth: 2});
+    }
+
+    showRadar(highestTraits, '#highest-traits', "Highest traits");
+
+    // 1 < value > 0
+    function getColour(value) {
+      var hue = ((1 - value) * 120).toString(10);
+      return ['hsl(',hue,',100%,50%)'].join('');
+    }
+
+    // show polar chart of artist traits
+    function showPolarArea(traits, selector) {
+      var labels = [], data = [], graph = []
+          , traitsCanvas = $$(selector)
+          , traitsCx = traitsCanvas.getContext('2d')
+          , traitsChart
+          , traitsLegend = $$('.traits-legend');
+
+      traits.map(function(trait) {
+        graph.push({
+          value: trait.percentage * 100
+          , color: getColour(trait.percentage)
+          , highlight: getColour(trait.percentage)
+          , label: trait.name
+        });
+      });
+
+      traitsChart = new Chart(traitsCx).PolarArea(graph, {responsive:true});
+
+      traitsLegend.innerHTML = traitsChart.generateLegend();
+    }
+    showPolarArea(traits, '#traits');
 });
